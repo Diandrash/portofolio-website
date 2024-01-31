eb7f842a0a84e3207064ae90a226274cd552d287
diandrash <fshadeva@gmail.com>
2024-01-31 21:23:05 +0700

    style(hero)edit text

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,7 +33,7 @@ function headerShadow() {
 
 /* ----- TYPING EFFECT ----- */
 var typingEffect = new Typed(".typedText",{
-  strings : ["Designer","Youtuber","Developer"],
+  strings : ["Front End","Back End","UI Designer", "Fullstack"],
   loop : true,
   typeSpeed : 100, 
   backSpeed : 80,
